09363ad6cc2d9aec65a874b973c1886caf0a29bf
Nicholas Studt <nicholas@nicholasstudt.com>
2008-12-29 21:58:28 +0000

    Updates.

--- views.py
+++ views.py
@@ -1,46 +1,71 @@
+import os
+import posixpath
 import urllib
 
 from django.http import Http404
 from django.shortcuts import render_to_response
-
-
-BASE_XPANDER_PATH = "/where/ever/"
+from django.utils.safestring import mark_safe
 
 # ALLOWED_INCLUDE_ROOTS <-- Require document root to be in this ?
 # Use MEDIA_ROOT as DOCUMENT_ROOT ?
 
-def serve(request, url, document_root=None):
-	"""
+def serve(request, url, document_root=None, require_auth=False):
+    """
 	Serve static content wrapped inside of the sites template.
 
 	To use, put a URL pattern such as: 
 
-		url(r'^(?P<path>.*.html)$', 'expander.serve',
+		url(r'^(?P<url>.*.html)$', 'expander.serve',
 			 	{'document_root' : '/path/to/my/files/'})
+
+    or
+
+		url(r'^(?P<url>.*.html)$', 'expander.serve',
+			 	{'document_root' : '/path/to/my/files/',
+			 	'require_auth' : True })
 
 	in your URLconf. The "document root" param must be provided, otherwise a
 	404 error will be raised.
-	"""
+    """
+    if document_root is None:
+        raise Http404, '"%s" does not exist' % url
 
-	if document_root is None:
-		raise Http404, '"%s" does not exist' % fullpath
+    # Decode all the % ecoded stuff.
+    path = posixpath.normpath(urllib.unquote(url))
+    path = path.lstrip('/')
+    newpath = ''
+    for part in path.split('/'):
+        if not part:
+            # Strip empty path components.
+            continue
+        drive, part = os.path.splitdrive(part)
+        head, part = os.path.split(part)
+        if part in (os.curdir, os.pardir):
+            # Strip '.' and '..' in path.
+            continue
+        newpath = os.path.join(newpath, part).replace('\\', '/')
+    
+    # Return a 404 if the path contains a "/../"
+    if newpath and path != newpath:
+        raise Http404, '"%s" does not exist' % newpath
 
-	# Decode all he % ecoded stuff.
-	newurl = urllib.unquote(url)
-	# Has this already been done to the URL ?
+    fullpath = os.path.join(document_root, newpath)
+    
+    # Send to auth if we require auth and not authenticated.
+    if require_auth and not request.user.is_authenticated():
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
 
-	# Return a 404 if the path contains a "/../"
-   
-    # url should be checked to verify it does not contain ".." in any
-    # form.
-    fname = os.path.join(document_root, url)
+    # Return a 404 if we don't exists
+    if not os.path.exists(fullpath):
+        raise Http404, '"%s" does not exist' % fullpath
 
-    # os.path.normpath(fname) # Works out the name removing ../../ and like
+    if not os.path.isfile(fullpath):
+        raise Http404, '"%s" does not exist' % fullpath
 
+    if not os.access(fullpath, os.R_OK):
+        raise Http404, '"%s" does not exist' % fullpath
 
-    if not os.path.exists(fname):
-        raise Http404
-    else:
-        return render_to_response( "expander/base.html", 
-                                    {'data': open(fname).read()}
-                                    )
+    # Finally, send the response to the user.
+    return render_to_response( "expander/base.html", 
+                                {'data': mark_safe(open(fullpath).read())} )

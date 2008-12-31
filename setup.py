06cbd092018ade3d98b24293f117b7952463ac2a
Nicholas Studt <nicholas@nicholasstudt.com>
2008-12-31 14:40:55 +0000

    Updates.

--- setup.py
+++ setup.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+application_dir = 'app-expander'
+  
+for dirpath, dirnames, filenames in os.walk(application_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
     name = "Expander",
@@ -8,9 +47,7 @@ setup(
     author = 'Nicholas Studt',
     author_email = 'nicholas@photodwarf.org',
     description = 'Apply templates to files on disk.',
-    package_dir = {'apps.expander': 'lib'},
-    packages = ['apps.expander'],
-
-#    scripts = ['django/bin/django-admin.py'],
+    packages = packages, 
+    data_files = data_files,
 )
 

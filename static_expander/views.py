edd4a65d89ce54770b3aac60ba50c9edbd09cd91
Nicholas Studt <nicholas@nicholasstudt.com>
2009-01-03 20:56:38 +0000

    Updates.

--- static_expander/views.py
+++ static_expander/views.py
@@ -1,16 +1,59 @@
 import mimetypes
-import os
-import stat
-import urllib
+import os, stat, urllib
 
+from django import http, template
 from django.conf import settings
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render_to_response
-from django.utils.safestring import mark_safe
 from django.utils.http import http_date
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy, ugettext as _
 
-# Todo: 
-# - Handle the auth correctly -- It does, just needs a login page now.
+ERROR_MESSAGE = ugettext_lazy("Please enter a correct username and password. Note that both fields are case-sensitive.")
+LOGIN_FORM_KEY = 'this_is_the_login_form'
+
+# _checklogin Based on _checklogin from django.contrib.admin.views.decorators
+def _checklogin(request, perms):
+    def _display_login_form(request, error_message=''):
+        request.session.set_test_cookie()
+        
+        return render_to_response('expander/login.html', {
+            'title': _('Log in'),
+            'app_path': request.get_full_path(),
+            'error_message': error_message
+            }, context_instance=template.RequestContext(request))
+
+    # If this isn't already the login page, display it.
+    if LOGIN_FORM_KEY not in request.POST:
+        if request.POST:
+            message = _("Please log in again, because your session has expired.")
+        else:
+            message = ""
+        return _display_login_form(request, message)
+
+    # Check that the user accepts cookies.
+    if not request.session.test_cookie_worked():
+        message = _("Looks like your browser isn't configured to accept cookies. Please enable cookies, reload this page, and try again.")
+        return _display_login_form(request, message)
+    else:
+        request.session.delete_test_cookie()
+
+    # Check the password.
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        return _display_login_form(request, ERROR_MESSAGE)
+    elif user.is_active and perms is None:
+        login(request, user)
+        return http.HttpResponseRedirect(request.get_full_path())
+    elif user.is_active and perms and not request.user.has_perms(perms):
+        login(request, user)
+        return http.HttpResponseRedirect(request.get_full_path())
+    else:
+        return _display_login_form(request, ERROR_MESSAGE)
+
 # - Should probably make this honor the not modified since stuff.
 def serve(request, url, document_root=None, require_auth=False, perms=None, 
           directory_index=('index.html'), extensions=('.html')):
@@ -34,7 +77,7 @@ def serve(request, url, document_root=None, require_auth=False, perms=None,
     files in a list.
     """
     if document_root is None:
-        raise Http404, '"%s" does not exist' % url
+        raise http.Http404, '"%s" does not exist' % url
 
     # Decode all the % ecoded stuff.
     path = os.path.normpath(urllib.unquote(url))
@@ -53,27 +96,25 @@ def serve(request, url, document_root=None, require_auth=False, perms=None,
     
     # Return a 404 if the path contains a "/../"
     if newpath and path != newpath:
-        raise Http404, '"%s" does not exist' % newpath
+        raise http.Http404, '"%s" does not exist' % newpath
 
     fullpath = os.path.join(document_root, newpath)
     
     # Send to auth if we require auth and not authenticated.
     if require_auth:
         if not request.user.is_authenticated():
-            from django.contrib.auth.views import redirect_to_login
-            return redirect_to_login(request.path)
-        elif perms is not None and not request.user.has_perms(perms):
-            from django.contrib.auth.views import redirect_to_login
-            return redirect_to_login(request.path)
+            return _checklogin(request, perms)
+        elif perms and not request.user.has_perms(perms):
+            return _checklogin(request, perms)
 
     # Return a 404 if we don't exists
     if not os.path.exists(fullpath):
-        raise Http404, '"%s" does not exist' % fullpath
+        raise http.Http404, '"%s" does not exist' % fullpath
 
     # If it's not a file then try adding default_page 
     if os.path.isdir(fullpath): 
         if not url.endswith('/') and settings.APPEND_SLASH:
-            return HttpResponseRedirect("%s/" % request.path)
+            return http.HttpResponseRedirect("%s/" % request.path)
 
         realpath = None
         for part in directory_index:
@@ -85,17 +126,18 @@ def serve(request, url, document_root=None, require_auth=False, perms=None,
    
     # Not a file or we can't read it.
     if not os.path.isfile(fullpath) or not os.access(fullpath, os.R_OK):
-        raise Http404, '"%s" does not exist' % fullpath
+        raise http.Http404, '"%s" does not exist' % fullpath
 
     if os.path.splitext(fullpath)[1] not in extensions:
         statobj = os.stat(fullpath)
         mimetype = mimetypes.guess_type(fullpath)[0] or 'application/octet-stream'
         contents = open(fullpath, 'rb').read()
-        response = HttpResponse(contents, mimetype=mimetype)
+        response = http.HttpResponse(contents, mimetype=mimetype)
         response["Last-Modified"] = http_date(statobj[stat.ST_MTIME])
         response["Content-Length"] = len(contents)
         return response
 
     # Finally, send the response to the user.
-    return render_to_response( "expander/base.html", 
-                               {'data': mark_safe(open(fullpath).read())} )
+    return render_to_response("expander/base.html", 
+                              {'data': mark_safe(open(fullpath).read())},
+                              context_instance=template.RequestContext(request))
